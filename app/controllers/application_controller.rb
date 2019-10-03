9940cb894e6e5068870794836584cfb65575c022
pururaj1908 <pururajdave@Pururaj.localdomain>
2019-10-03 02:49:19 -0400

    Removed after sign_in redirection method from application level controller to its child user controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-    def after_sign_in_path_for(resource)
-                if resource_class == Student
-                        student_path(current_student)
-                end
-		if resource_class == Librarian
-                        librarian_path(current_librarian)
-                end
-    end	
 end
