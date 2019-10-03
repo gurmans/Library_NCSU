7c85f76c9200b2760c32f3769b19ba051ec03b03
pururaj1908 <pururajdave@Pururaj.localdomain>
2019-10-03 03:01:13 -0400

    added after_sign_in method inside student session controller to handle proper redirection and added admin authentication in student controller

--- app/controllers/students/sessions_controller.rb
+++ app/controllers/students/sessions_controller.rb
@@ -17,10 +17,13 @@ class Students::SessionsController < Devise::SessionsController
     super
   end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def after_sign_in_path_for(resource)
+      pages_studenthome_path()   
+  end	
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :authenticate_admin!, only: [:show, :edit, :update, :destroy]
   before_action :set_student, only: [:show, :edit, :update, :destroy]
 
   # GET /students
