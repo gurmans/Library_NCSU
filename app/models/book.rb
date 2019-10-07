1d7b4c7c991c38bf240b09331e41cbde31d486ce
pururaj1908 <pururajdave@Pururaj.localdomain>
2019-10-06 21:49:07 -0400

    added helper methods for checkout/return book functionality

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,22 @@ class Book < ApplicationRecord
   belongs_to :student, optional: true
   belongs_to :library
   has_many :hold_book_trackers, :dependent => :destroy
+
+  def self.createNewCheckoutEntry?(bookid,studentid)
+	currentDate = Date.current
+	bookHistory = BookHistory.new(:issueDate=>currentDate,:book_id=>bookid,:student_id=>studentid)
+	bookHistory.save	
+  end
+
+  def self.updateExistingCheckoutEntry?(bookid,studentid)
+	currentDate = Date.current
+	bookHistory = BookHistory.where(:returnDate=>nil,:book_id=>bookid,:student_id=>studentid)
+	bookHistory.update(:returnDate=>currentDate)	
+  end 
+
+  def self.updateAvailableCounter?(bookid,adder)
+	updateBook = Book.find(bookid)
+        availableCounter = updateBook.available + adder
+	updateBook.update(:available=>availableCounter)
+  end	 	
 end
--- app/models/book_history.rb
+++ app/models/book_history.rb
@@ -1,4 +1,13 @@
 class BookHistory < ApplicationRecord
   belongs_to :book
   belongs_to :student
+  
+  def self.checkIfAlreadyIssued?(bookid,studentid)
+       isIssued = BookHistory.where(:returnDate=>nil,:book_id=>bookid,:student_id=>studentid)
+       if isIssued.count > 0
+          return true
+       else
+          return false		
+       end    		
+  end	  
 end
