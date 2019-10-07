a3ba638acbd550f7f73115c01832fb816f7a0d9f
pururaj1908 <pururajdave@Pururaj.localdomain>
2019-10-07 05:38:06 -0400

    Added bookhistory instance variable to render checked out books list partial view on user home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,18 +6,25 @@ class PagesController < ApplicationController
 
   def adminhome
 	if !admin_signed_in?
-		redirect_to root_path
-	end
+		redirect_to root_path	
+	else
+        	@book_histories = BookHistory.GetBookHistory(:admin)
+	end 
   end
 
   def studenthome
 	if !student_signed_in?
-                redirect_to root_path                                                                                           end
+                redirect_to root_path
+	else
+		@book_histories = BookHistory.GetBookHistory(:student,current_student)
+	end
   end
 
   def librarianhome
 	if !librarian_signed_in?
-                redirect_to root_path                                                                                           end
- 
+                redirect_to root_path  
+	else
+		@book_histories = BookHistory.GetBookHistory(:librarian,current_librarian)
+ 	end
   end
 end
