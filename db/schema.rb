99f3788047f44f2095e576cabde750c0bd11a195
pururaj1908 <pururajdave@Pururaj.localdomain>
2019-10-06 21:53:36 -0400

    modified routes and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_075205) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 2019_10_06_230337) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,8 +30,8 @@ ActiveRecord::Schema.define(version: 2019_10_06_075205) do
     t.date "returnDate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "book_id"
-    t.bigint "student_id"
+    t.integer "book_id"
+    t.integer "student_id"
     t.index ["book_id"], name: "index_book_histories_on_book_id"
     t.index ["student_id"], name: "index_book_histories_on_student_id"
   end
@@ -49,11 +46,11 @@ ActiveRecord::Schema.define(version: 2019_10_06_075205) do
     t.string "image"
     t.text "summary"
     t.boolean "specialCollection"
-    t.date "returnDate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "student_id"
-    t.bigint "library_id"
+    t.integer "student_id"
+    t.integer "library_id"
+    t.integer "available"
     t.index ["library_id"], name: "index_books_on_library_id"
     t.index ["student_id"], name: "index_books_on_student_id"
   end
@@ -74,7 +71,7 @@ ActiveRecord::Schema.define(version: 2019_10_06_075205) do
     t.string "bookmarks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "library_id"
+    t.integer "library_id"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -91,7 +88,7 @@ ActiveRecord::Schema.define(version: 2019_10_06_075205) do
     t.decimal "overdueFine"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "university_id"
+    t.integer "university_id"
     t.index ["university_id"], name: "index_libraries_on_university_id"
   end
 
@@ -116,8 +113,8 @@ ActiveRecord::Schema.define(version: 2019_10_06_075205) do
     t.string "bookmarks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "program_id"
-    t.bigint "university_id"
+    t.integer "program_id"
+    t.integer "university_id"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -140,12 +137,4 @@ ActiveRecord::Schema.define(version: 2019_10_06_075205) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "book_histories", "books"
-  add_foreign_key "book_histories", "students"
-  add_foreign_key "books", "libraries"
-  add_foreign_key "books", "students"
-  add_foreign_key "librarians", "libraries"
-  add_foreign_key "libraries", "universities"
-  add_foreign_key "students", "programs"
-  add_foreign_key "students", "universities"
 end
