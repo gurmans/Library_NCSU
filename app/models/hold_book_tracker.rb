1bd7f85ed1298ef37a892b7d6fb20ae32d670c37
pururaj1908 <pururajdave@Pururaj.localdomain>
2019-10-09 15:29:13 -0400

    added method to get hold requests and special collection views and approve/deny special collection requests

--- app/models/hold_book_tracker.rb
+++ app/models/hold_book_tracker.rb
@@ -2,12 +2,32 @@ class HoldBookTracker < ApplicationRecord
   belongs_to :book
   belongs_to :student
 
-  def approve_special_collection_requests?(bookid, studentid)
-    if Book.find(bookid).specialCollection
-      Book.createNewCheckoutEntry?(bookid,studentid)
-      Book.updateAvailableCounter?(bookid,-1)
-      return HoldBookTracker.find_by(:bookid=>bookid,:studentid=>studentid)&.destroy
+  def self.GetBookHoldRequests(type,user = nil,isSpecialCollectionView = false)
+	case type
+	when :admin
+		book_hold_requests = HoldBookTracker.all
+	when :librarian
+		if user
+			if isSpecialCollectionView
+				book_hold_requests = HoldBookTracker.where(:book_id=>Book.where(:library_id=>user.library.id,:specialCollection=>true))
+			else
+				book_hold_requests = HoldBookTracker.where(:book_id=>Book.where(:library_id=>user.library.id))
+			end	
+		end	
     end
-    return true
+	return book_hold_requests
+  end
+  
+  def self.handleSpecialCollectionRequest?(bookid,studentid,approve)
+	if Book.find(bookid).specialCollection
+	  if approve
+		Book.createNewCheckoutEntry?(bookid,studentid)
+		Book.updateAvailableCounter?(bookid,-1)
+		return HoldBookTracker.find_by(:book_id=>bookid,:student_id=>studentid)&.destroy
+	  else
+		return HoldBookTracker.find_by(:book_id=>bookid,:student_id=>studentid)&.destroy
+	  end	
+    end
+    return false	
   end
 end
